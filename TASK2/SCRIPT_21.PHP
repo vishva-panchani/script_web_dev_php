1704e69d201e2345ab81edfc6a5222983779b915
Vishva Panchani <143687199+vishva-panchani@users.noreply.github.com>
2023-10-27 15:07:07 +0530

     Script_21 Script to check if a given number is a palindrome or not and display the result.

---
+++ TASK2/SCRIPT_21.PHP
@@ -0,0 +1,12 @@
+<?php
+//palindrome numbers are number which are same from reverse example 4554,1221
+$num=232 ;
+$num1=strrev($num);
+if($num1==$num){
+    echo "The Number is Palindrome";
+}
+else
+{
+    echo "The Number is not Palindrome";
+}
+?>
