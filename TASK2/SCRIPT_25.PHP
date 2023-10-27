9d33fa725efb1853e59c87f6ee3c24769ab64a27
Vishva Panchani <143687199+vishva-panchani@users.noreply.github.com>
2023-10-27 15:44:53 +0530

    Script_25 Script to swap 2 nos without using 3rd variable using arithmetic operators.

---
+++ TASK2/SCRIPT_25.PHP
@@ -0,0 +1,13 @@
+<?php
+
+$i=5;
+    echo "The value of num1 is $i<br/>";
+$j= 3;
+    echo "The value of num2 is $j<br/>";
+$i=$i+$j;
+$j=$i-$j;
+    echo "after swapping  num2 is $j<br/>";
+$i=$i-$j;
+    echo "after swapping num1  is $i<br/>";
+
+?>
